4aeae8648f1872c5d59937885b5892d062d074e7
romina <romirodrigez13@gmail.com>
2024-10-04 20:35:10 -0300

    Agregando clase5 de javaScrip PERN-Stack

---
+++ PERN-Stack/src/controllers/tareas.controllers.js
@@ -0,0 +1,10 @@
+export const listarTareas = (req, res) => res.send("Obteniendo tareas");
+
+export const listarTrea = (req, res) => res.send("Obteniendo tarea única");
+
+export const crearTarea = (req, res) => res.send("Creando tarea");
+
+export const actualizarTarea = (req, res) => res.send("Actualizando tarea unica");
+
+export const eliminarTarea = (req, res) => res.send("Eliminando tarea unica");
+
