abd04be93b0aeaa763a7d25fbc7e743b23dbf284
virginiacastellano <vcastellano140@gmail.com>
2024-10-10 11:21:01 -0300

    se sube el proyecto final

---
+++ Proyect_final/client/js/products.js
@@ -0,0 +1,59 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Blusa",
+        price: 25,
+        quanty: 1,
+        img: "media/blusa.png",
+    },
+    {
+        id: 2,
+        productName: "Vestido",
+        price: 45,
+        quanty: 1,
+        img: "media/vestido.png",
+    },
+    {
+        id: 3,
+        productName: "Jeans",
+        price: 50,
+        quanty: 1,
+        img: "media/jeans.png",
+    },
+    {
+        id: 4,
+        productName: "Vestido de verano",
+        price: 40,
+        quanty: 1,
+        img: "media/sundress.jpg",
+    },
+    {
+        id: 5,
+        productName: "Camiseta",
+        price: 20,
+        quanty: 1,
+        img: "media/tshirt.jpg",
+    },
+    {
+        id: 6,
+        productName: "Suéter",
+        price: 55,
+        quanty: 1,
+        img: "media/sweater.png",
+    },
+    {
+        id: 7,
+        productName: "Falda",
+        price: 35,
+        quanty: 1,
+        img: "media/skirt.png",
+    },
+    {
+        id: 8,
+        productName: "Chaqueta",
+        price: 70,
+        quanty: 1,
+        img: "media/jacket.png",
+    },
+
+];
